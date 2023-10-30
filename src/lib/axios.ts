ca7aa0c2766b3e6fdc96f2f1a18d2047086cc87c
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2023-10-30 21:49:11 +0630

    csrf fix_2

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,9 +5,9 @@ const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
 const axios = Axios.create({
     withCredentials: true,
     headers: {
-        'X-Requested-With': 'XMLHttpsRequest',
+        'X-Requested-With': 'XMLHttpRequest',
     },
-    baseURL: baseURL
+    baseURL: baseURL,
 })
 
 export const csrf = () => axios.get(baseURL + '/sanctum/csrf-cookie')
