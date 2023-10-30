7cc53fe5cd385eae93931f04e520a3e41e1b0618
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2023-10-30 21:32:06 +0630

    csrf fix_1

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,22 +1,14 @@
-import Axios from "axios"
+import Axios from 'axios'
 
 const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const axios = Axios.create({
     withCredentials: true,
     headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-       // 'X-XSRF-TOKEN':'XSRF-TOKEN'
-      // 'Access-Control-Allow-Credentials': 'true',
-      //  'Access-Control-Allow-Origin':'*',
-      //  'Access-Control-Allow-Methods' : 'GET,DELETE,PATCH,POST,PUT',
-      //  'Access-Control-Allow-Headers' :'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        'X-Requested-With': 'XMLHttpsRequest',
     },
     baseURL: baseURL
 })
-
-//SESSION_DOMAIN=localhost
-//SANCTUM_STATEFUL_DOMAINS=localhost
 
 export const csrf = () => axios.get(baseURL + '/sanctum/csrf-cookie')
 
