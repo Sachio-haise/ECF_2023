6ebae0a5b2f613adab2bdec0a3a7ed5d8b695efc
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2023-10-30 22:55:58 +0630

    https fix

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,7 +5,12 @@ const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
 const axios = Axios.create({
     withCredentials: true,
     headers: {
-        'X-Requested-With': 'XMLHttpsRequest',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-XSRF-TOKEN':'XSRF-TOKEN',
+         'Access-Control-Allow-Credentials': 'true',
+      //  'Access-Control-Allow-Origin':'*',
+      //  'Access-Control-Allow-Methods' : 'GET,DELETE,PATCH,POST,PUT',
+      //  'Access-Control-Allow-Headers' :'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     },
     baseURL: baseURL,
 })
